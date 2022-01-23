8e8fa616cff58165081a63b71fbc6eb69fff5929
Meenatchi Sundaram Muthu Selva Annamalai <msm518@ic.ac.uk>
2022-01-23 13:56:27 +0000

    Removed unnecessary code and modified metadata

--- vue.config.js
+++ vue.config.js
@@ -11,18 +11,3 @@ process.env.VUE_APP_BUILD_ID = process.env.BUILD_ID;
 fs.writeFileSync('public/build_id.json', JSON.stringify({
   BUILD_ID: process.env.BUILD_ID
 }));
-
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      // On ne s'intéresse qu'à la timezone de Paris
-      new MomentTimezoneDataPlugin({
-        matchZones: /(CET|CEST|Paris)$/,
-        startYear: currentYear - 1,
-        endYear: currentYear + 5,
-    }),
-      // Et on n'a pas besoin des autres langues de momentjs
-      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fr/),
-    ]
-  }
-};
